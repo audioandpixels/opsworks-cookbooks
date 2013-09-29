15ef01c6249fc7c7eedae4f715972fc41181cc2c
jasoncox <jason@audioandpixels.com>
2013-09-29 11:44:04 +1000

    Initial commit

---
+++ opsworks_sidekiq/recipes/restart.rb
@@ -0,0 +1,17 @@
+node[:deploy].each do |application, deploy|
+  bash 'restart sidekiq' do
+    code <<-EOH
+    sleep 1
+    /usr/bin/god restart workers
+    EOH
+    user 'root'
+    action :nothing
+  end
+
+  ruby_block 'restart sidekiq later' do
+    block do
+      true
+    end
+    notifies :run, resources(:bash => 'restart sidekiq'), :delayed
+  end
+end
