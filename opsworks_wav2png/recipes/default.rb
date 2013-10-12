698d00059c1a634730a6a53f89972abbd593e154
jasoncox <jason@audioandpixels.com>
2013-10-12 18:58:50 +1100

    Run sudo

--- opsworks_wav2png/recipes/default.rb
+++ opsworks_wav2png/recipes/default.rb
@@ -1,6 +1,6 @@
 # Move wav2png after rails app deploy
 bash "Move wav2png to PATH bin location" do
-  code "cp /mnt/srv/www/audiometric_app/current/vendor/wav2png/bin/wav2png /usr/bin/wav2png"
-  code "chmod 755 /usr/bin/wav2png"
-  code "chown root:root /usr/bin/wav2png"
+  code "sudo cp /mnt/srv/www/audiometric_app/current/vendor/wav2png/bin/wav2png /usr/bin/wav2png"
+  code "sudo chmod 755 /usr/bin/wav2png"
+  code "sudo chown root:root /usr/bin/wav2png"
 end
