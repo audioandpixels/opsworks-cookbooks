43a0d7f639fa11b7a84c2dab912dc14ac67f01cd
jasoncox <jason@audioandpixels.com>
2013-10-13 12:19:53 +1100

    Add deps to wav2png

--- opsworks_wav2png/recipes/default.rb
+++ opsworks_wav2png/recipes/default.rb
@@ -1,6 +1,15 @@
+# Install package dependencies for wav2png
+apt_package "libpng++-dev" do
+	action :install
+end
+
+apt_package "libboost-program-options-dev" do
+	action :install
+end
+
 # Move wav2png after rails app deploy
 bash "Move wav2png to PATH bin location" do
-  code "sudo cp /mnt/srv/www/audiometric_app/current/vendor/wav2png/bin/wav2png /usr/bin/wav2png"
-  code "sudo chmod 755 /usr/bin/wav2png"
-  code "sudo chown root:root /usr/bin/wav2png"
+  code "cp /mnt/srv/www/audiometric_app/current/vendor/wav2png/bin/wav2png /usr/bin/wav2png"
+  code "chmod 755 /usr/bin/wav2png"
+  code "chown root:root /usr/bin/wav2png"
 end
