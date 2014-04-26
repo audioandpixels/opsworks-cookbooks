374d3bb1865ce9c25a5e5127adb40744080be972
jasoncox <jason@audioandpixels.com>
2014-04-26 15:34:05 +1000

    Precomp assets recipe

--- rails/recipes/precomp_assets.rb
+++ rails/recipes/precomp_assets.rb
@@ -1,9 +1,13 @@
-include_recipe "deploy"
+node[:deploy].each do |application, deploy|
+  rails_env = deploy[:rails_env]
+  current_path = deploy[:current_path]
 
-Chef::Log.info("Precompiling assets for RAILS_ENV=production...")
+  Chef::Log.info("Precompiling Rails assets with environment #{rails_env}")
 
-execute "rake assets:precompile" do
-  cwd deploy[:current_path]
-  command "bundle exec rake assets:precompile"
-  environment "RAILS_ENV" => "production"
+  execute 'rake assets:precompile' do
+    cwd current_path
+    user 'deploy'
+    command 'bundle exec rake assets:precompile'
+    environment 'RAILS_ENV' => rails_env
+  end
 end
