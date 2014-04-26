3139205a9401c65f6632b8547aeeaa0b444d3372
jasoncox <jason@audioandpixels.com>
2014-04-26 13:51:32 +1000

    Tweak precomp recipe

--- rails/recipes/precomp_assets.rb
+++ rails/recipes/precomp_assets.rb
@@ -1,9 +1,7 @@
-rails_env = new_resource.environment["RAILS_ENV"]
-
-Chef::Log.info("Precompiling assets for RAILS_ENV=#{rails_env}...")
+Chef::Log.info("Precompiling assets for RAILS_ENV=production...")
 
 execute "rake assets:precompile" do
   cwd release_path
   command "bundle exec rake assets:precompile"
-  environment "RAILS_ENV" => rails_env
+  environment "RAILS_ENV" => "production"
 end
