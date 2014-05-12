c88839f893acf857e1836a88823e2da58bc7b47f
Jason Cox <jason@audioandpixels.com>
2014-05-12 15:24:01 +1000

    Update remote_syslog version

--- opsworks_papertrail/attributes/default.rb
+++ opsworks_papertrail/attributes/default.rb
@@ -1,4 +1,4 @@
-default[:papertrail][:remote_syslog][:version] = '1.6.5'
+default[:papertrail][:remote_syslog][:version] = '1.6.14'
 default[:papertrail][:host] = 'logs.papertrailapp.com'
 default[:papertrail][:port] = '514'
 default[:papertrail][:logs] = []
