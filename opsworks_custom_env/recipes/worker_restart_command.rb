accac9c5ea036fff6edc90d348f45866b7940f70
Jason Cox <jason@audioandpixels.com>
2014-06-01 14:11:23 +1000

    Update worker_restart_command.rb

--- opsworks_custom_env/recipes/worker_restart_command.rb
+++ opsworks_custom_env/recipes/worker_restart_command.rb
@@ -1,7 +1,7 @@
 node[:deploy].each do |application, deploy|
 
   execute "restart Rails app #{application} for custom env" do
-    command "command "sleep 300 && sudo monit restart sidekiq_#{application}"
+    command "sleep 300 && sudo monit restart sidekiq_#{application}"
     action :nothing
   end
 
