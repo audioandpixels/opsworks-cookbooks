7c2e770d814e6ebc6302b2aa4bf4b81a41b6a4a7
Jason Cox <jason@audioandpixels.com>
2014-06-04 19:18:56 +1000

    Update precomp_assets.rb

--- rails/recipes/precomp_assets.rb
+++ rails/recipes/precomp_assets.rb
@@ -5,14 +5,15 @@ node[:deploy].each do |application, deploy|
     deploy = node[:deploy][application]
     rails_env = deploy[:rails_env]
 
-    Chef::Log.info("Cleaning up Rails assets with environment #{rails_env}")
+    # Zero downtime - need to leave old assets around
+    # Chef::Log.info("Cleaning up Rails assets with environment #{rails_env}")
 
-    execute 'rake assets:clobber' do
-      cwd deploy[:current_path]
-      user 'deploy'
-      command 'bundle exec rake assets:clobber'
-      environment 'RAILS_ENV' => rails_env
-    end
+    # execute 'rake assets:clobber' do
+    #   cwd deploy[:current_path]
+    #   user 'deploy'
+    #   command 'bundle exec rake assets:clobber'
+    #   environment 'RAILS_ENV' => rails_env
+    # end
   
     Chef::Log.info("Precompiling Rails assets with environment #{rails_env}")
 
