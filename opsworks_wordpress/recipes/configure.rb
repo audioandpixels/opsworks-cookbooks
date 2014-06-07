87d52f190a42401a8d531841b3168104d467de9b
jasoncox <jason@audioandpixels.com>
2014-06-07 15:27:36 +1000

    Add opsworks_wordpress

---
+++ opsworks_wordpress/recipes/configure.rb
@@ -0,0 +1,48 @@
+# AWS OpsWorks Recipe for Wordpress to be executed during the Configure lifecycle phase
+# - Creates the config file wp-config.php with MySQL data.
+# - Creates a Cronjob.
+# - Imports a database backup if it exists.
+
+require 'uri'
+require 'net/http'
+require 'net/https'
+
+uri = URI.parse("https://api.wordpress.org/secret-key/1.1/salt/")
+http = Net::HTTP.new(uri.host, uri.port)
+http.use_ssl = true
+http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+request = Net::HTTP::Get.new(uri.request_uri)
+response = http.request(request)
+keys = response.body
+
+# Create the Wordpress config file wp-config.php with corresponding values
+node[:deploy].each do |app_name, deploy|
+
+    template "#{deploy[:deploy_to]}/current/wp-config.php" do
+        source "wp-config.php.erb"
+        mode 0660
+        group deploy[:group]
+
+        if platform?("ubuntu")
+          owner "www-data"
+        elsif platform?("amazon")
+          owner "apache"
+        end
+
+        variables(
+            :database   => (deploy[:database][:database] rescue nil),
+            :user       => (deploy[:database][:username] rescue nil),
+            :password   => (deploy[:database][:password] rescue nil),
+            :host       => (deploy[:database][:host] rescue nil),
+            :keys       => (keys rescue nil)
+        )
+    end
+end
+
+# Create a Cronjob for Wordpress
+cron "wordpress" do
+  hour "*"
+  minute "*/15"
+  weekday "*"
+  command "wget -q -O - http://localhost/wp-cron.php?doing_wp_cron >/dev/null 2>&1"
+end
